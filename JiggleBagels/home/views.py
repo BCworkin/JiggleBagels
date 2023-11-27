05f08096685dded2bce91bdfffef7fb55b8b9235
Bryan's Project 101s <bryanchiamjf01@gmail.com>
2023-11-27 23:04:04 +1030

    setup admin backend, display items/categories

--- JiggleBagels/home/views.py
+++ JiggleBagels/home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from items.models import Item, Category
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    
+    return render(request, 'home/index.html', {
+        'categories': categories, 
+        'items': items,
+    })
 
 # not sure about this yet
 def contact(request):
--- JiggleBagels/items/models.py
+++ JiggleBagels/items/models.py
@@ -3,3 +3,21 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+    
+class Item(models.Model):
+    category = models.ForeignKey(Category, related_name = 'items' , on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank = True, null = True)
+    price = models.FloatField()
+    is_sold = models.BooleanField(default=False)
+    image = models.ImageField( upload_to='item_images', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
